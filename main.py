c93fcfea0c8e7c82697ba889d6ea0f700436b739
NEngland24 <824864@mystma.org>
2023-03-27 14:42:14 -0500

    4

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 #WRITE YOUR CODE IN THIS FILE
-def topTenMovies(a, b, c, d, e):
-    a == "whopper"
-    b == "whopper"
-    c == "whopper"
-    d == "whopper"
-    e == "whopper"
-    return a, b, c, d, e
+def topTenMovies():
+    return "a " + "b " + "c " + "d " + "e"
